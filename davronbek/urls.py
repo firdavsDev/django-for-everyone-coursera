f08685ddb5a98c6bc30b74901d1f4bc2e13fa9b0
Davronbek Boltayev <xackercoder@gmail.com>
2023-12-18 04:35:59 +0500

    aded crud

--- davronbek/urls.py
+++ davronbek/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('hello.urls', namespace='hello')),
+    # path('', include('hello.urls', namespace='hello')),
+    path('', include('home.urls')),  # Change to ads.urls
     path('admin/', admin.site.urls),
     path('polls/', include('quiz.urls', namespace='quiz')),
+    
+    path('accounts/', include('django.contrib.auth.urls')),  # Keep
+
+    # Sample applications
+    path('autos/', include('autos.urls')),
 ]
