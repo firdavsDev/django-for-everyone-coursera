3e3b1302ae25ec4f59b3403ef04aec3995ea035a
Davronbek Boltayev <xackercoder@gmail.com>
2023-12-18 04:09:49 +0500

    aded

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 def cookie(request):
     print(request.COOKIES)
     oldval = request.COOKIES.get('zap', None)
-    resp = HttpResponse('In a view - the zap cookie value is '+str(oldval))
+    resp = HttpResponse(f'In a view - the zap cookie value is {str(oldval)}')
     if oldval :
         resp.set_cookie('zap', int(oldval)+1) # No expired date = until browser close
     else :
@@ -18,10 +18,10 @@ def cookie(request):
 
 # https://www.youtube.com/watch?v=Ye8mB6VsUHw
 
-def sessfun(request) :
+def sessfun(request):
     num_visits = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_visits
     if num_visits > 4 : del(request.session['num_visits'])
-    resp = HttpResponse('view count='+str(num_visits))
-    resp.set_cookie('dj4e_cookie', '4ac77841', max_age=1000)
+    resp = HttpResponse(f'view count={str(num_visits)}')
+    resp.set_cookie('dj4e_cookie', '71985d37', max_age=1000)
     return resp
