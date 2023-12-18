45743b0a5fbdf0b9fdbd882f9bcd736c5e430bce
Davronbek Boltayev <xackercoder@gmail.com>
2023-12-18 11:03:44 +0500

    added

--- davronbek/urls.py
+++ davronbek/urls.py
@@ -9,9 +9,9 @@ from .views import logout_user
 urlpatterns = [
     path('', include('ads.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/logout/', logout_user, name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),  # Keep
     #logout
-    path('accounts/logout/', logout_user, name='logout'),
     path('oauth/', include('social_django.urls', namespace='social')),  # Keep
 
     # Sample applications
