f0af248389973afcf12bdb5d8f6cf6b85101f672
Davronbek Boltayev <xackercoder@gmail.com>
2023-12-18 12:14:00 +0500

    added

--- davronbek/urls.py
+++ davronbek/urls.py
@@ -1,8 +1,10 @@
 import os
 
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LogoutView
+from django.urls import path, include, re_path
+# from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
 
 from .views import logout_user
 
@@ -23,6 +25,37 @@ urlpatterns = [
     # path('', include('home.urls')),  # Change to ads.urls
 
 ]
+
+
+# Serve the static HTML
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    re_path(r'^site/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
+        name='site_path'
+        ),
+]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
+]
+
+# Switch to social login if it is configured - Keep for later
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0,
+                       path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+                       )
+    print('Using', social_login, 'as the login template')
+except:
+    print('Using registration/login.html as the login template')
 
 # References
 
